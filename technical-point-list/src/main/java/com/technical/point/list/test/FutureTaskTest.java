2b3c95747e663b1419661c933fb60776038572f6
Mr.Gao <gaogba@163.com>
2021-12-07 14:17:37 +0800

    补充

--- technical-point-list/src/main/java/com/technical/point/list/test/FutureTaskTest.java
+++ technical-point-list/src/main/java/com/technical/point/list/test/FutureTaskTest.java
@@ -1,8 +1,6 @@
 package com.technical.point.list.test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author: Mr.Gao
@@ -10,6 +8,7 @@ import java.util.concurrent.TimeUnit;
  * @description: 测试FutureTask
  * <p>
  * 1、验证构造方法，public FutureTask(Runnable runnable,V result)
+ * 2、FutureTask如果计算尚未完成，则get方法将阻止
  * </p>
  */
 public class FutureTaskTest {
@@ -43,6 +42,34 @@ public class FutureTaskTest {
         //主线程模拟延时
 //        TimeUnit.SECONDS.sleep(3);
 //        System.out.println(futureTask.get());
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        final Future<Boolean> future = executorService.submit(() -> {
+            //模拟业务逻辑处理
+            System.out.println("Executors future 需要业务逻辑执行..");
+            TimeUnit.SECONDS.sleep(3);
+            return true;
+        });
+        System.out.println(future.get());
+        final Future<Boolean> future2 = executorService.submit(() -> {
+            //模拟业务逻辑处理
+            System.out.println("Executors future2 需要业务逻辑执行..");
+            TimeUnit.SECONDS.sleep(5);
+            return true;
+        });
+        System.out.println(future2.get());
+
+        final Future<Boolean> future3 = executorService.submit(() -> {
+            //模拟业务逻辑处理
+            System.out.println("Executors future3 需要业务逻辑执行..");
+            TimeUnit.SECONDS.sleep(1);
+            return true;
+        });
+        System.out.println(future3.get());
+
+        //关闭
+        executorService.shutdown();
+
     }
 }
 
