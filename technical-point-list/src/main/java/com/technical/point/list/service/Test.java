ca5d1574ababcf3cac84e010235650c0649571fd
Mr.Gao <gaogba@163.com>
2022-03-04 16:23:43 +0800

    新增从容器中获取bean对象测试案例

---
+++ technical-point-list/src/main/java/com/technical/point/list/service/Test.java
@@ -0,0 +1,18 @@
+package com.technical.point.list.service;
+
+import com.technical.point.list.service.config.ScanConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author: Mr.Gao
+ * @date: 2022年03月04日 13:49
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ScanConfig.class);
+        PersonTestService personService = context.getBean(PersonTestService.class);
+        personService.getBeanName("studentService");
+    }
+}
---
+++ technical-point-list/src/main/java/com/technical/point/list/service/config/ScanConfig.java
@@ -0,0 +1,14 @@
+package com.technical.point.list.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Mr.Gao
+ * @date: 2022年03月04日 13:47
+ * @description:
+ */
+@Configuration
+@ComponentScan(basePackages = "com.technical.point.list.service")
+public class ScanConfig {
+}
